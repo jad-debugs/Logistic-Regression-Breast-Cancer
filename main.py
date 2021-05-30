dd66d5a06acbf6d852bd501d849015a65810aac2
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-30 16:09:31 -0700

    making pandas df

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import pandas as pd
+import numpy as np
 from sklearn.datasets import load_breast_cancer
 
 cancer_data = load_breast_cancer()
 
-print(cancer_data["DESCR"])
+# we learn that malignant is cancerous, and benign is not concerous
+
+df = pd.DataFrame(cancer_data['data'], columns = cancer_data['feature_names'])
+print(df.head())
